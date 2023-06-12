7d2a8c17c187d33cea889218f67389ff59306968
SonghaiFan <songhai.fan2022@gmail.com>
2023-06-12 21:19:17 +1000

    add ui

---
+++ ui/display.js
@@ -0,0 +1,25 @@
+// display.js
+// Define setup and configuration for data display
+
+// Function to create data display
+function createDisplay() {
+  // ... code to create initial data display
+}
+
+// Function to update data display
+function updateDisplay(data) {
+  // ... code to update display with new data
+  console.log("Display updated with data: " + data);
+}
+
+// Listen for postMessages from other scripts
+window.addEventListener("message", function (event) {
+  // Check the event type
+  if (event.data.type && event.data.type === "DATA_SELECTED") {
+    // If the event contains a data selection, update the display
+    updateDisplay(event.data.data);
+  }
+});
+
+// Run display creation function
+createDisplay();
